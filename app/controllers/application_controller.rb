eb5719b7f084e1cb3e7dd82210387829aef7c5f9
RyanVerhey <ryan90v@gmail.com>
2014-06-04 12:25:10 -0500

    Email is downcased in #user_params so it is downcased when saved to the DB

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,13 @@ class ApplicationController < ActionController::Base
   end
 
   def user_params
-    [:first_name, :last_name, :twitter_handle, :email, :bio, :interests, :gravatar_url].inject({}) { |hash, attribute| hash.merge(attribute => params[attribute]) }
+    [:first_name, :last_name, :twitter_handle, :email, :bio, :interests, :gravatar_url].inject({}) do |hash, attribute|
+      if attribute == :email
+        hash.merge(attribute => params[attribute].downcase)
+      else
+        hash.merge(attribute => params[attribute])
+      end
+    end
   end
 
 end
